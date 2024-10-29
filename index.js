2c2ed672b333a7a8ea9d704dfbb01a1616800d9b
Egglol1 <24egglal@gmail.com>
2024-10-29 13:48:59 -0400

    exposed static documentations page

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ let topMovies = [
 ];
 
 app.use(morgan('combined', {stream: accessLogStream}));
+app.use(express.static('/documentation', 'public'));
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
