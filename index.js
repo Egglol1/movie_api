b1e9a2b5ef2d7967a237abfa98872ea192efd5f6
Egglol1 <24egglal@gmail.com>
2024-11-17 13:15:17 -0500

    Now connects to database externally through mongoDB Atlas

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
-mongoose.connect('mongodb://localhost:27017/cfBD', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/cfBD', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
 const bcrypt = require('bcrypt');
