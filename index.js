6c834b896ed3758b7d626bce36eb6909181ca160
Egglol1 <24egglal@gmail.com>
2024-11-17 10:38:55 -0500

    Added CORS policy

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const Users = Models.User;
 
 mongoose.connect('mongodb://localhost:27017/cfBD', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const cors = require('cors');
+
 app.use(morgan('common'));
 app.use(bodyParser.urlencoded({
   extended: true
@@ -20,6 +22,19 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static('public'));
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
