b4454238b808cfd32154964dcd51f7952b40e8ea
Egglol1 <24egglal@gmail.com>
2024-12-16 18:51:33 -0500

    Added local usage to CORS

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static('public'));
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
