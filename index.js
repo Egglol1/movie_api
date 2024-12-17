0bba615f84b7c642f95bc86705b275f79b5f58b0
Egglol1 <24egglal@gmail.com>
2024-12-16 19:10:28 -0500

    Still attempting to turn off CORS

--- index.js
+++ index.js
@@ -26,16 +26,17 @@ app.use(methodOverride());
 app.use(express.static('public'));
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message ), true); //CHANGE TO FALSE LATER ALEX
-    }
-    return callback(null, true);
-  }
-}));
+//UNCOMMENT AFTER TESTING
+//app.use(cors({
+//  origin: (origin, callback) => {
+//    if(!origin) return callback(null, true);
+//    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+//      return callback(new Error(message ), false);
+//    }
+//    return callback(null, true);
+//  }
+//}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
