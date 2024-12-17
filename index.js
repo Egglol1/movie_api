e295b04541f660bd82848a69d9a709f686a8c26b
Egglol1 <24egglal@gmail.com>
2024-12-17 12:55:49 -0500

    Added console.log to assist debugging

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use(express.static('public'));
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
+    console.log('Origin: ', origin)
+    if(!origin || allowedOrigins.includes(origin)) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
       return callback(new Error(message ), false);
