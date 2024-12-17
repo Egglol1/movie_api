4c8cd8069744fe99734840ac3703dc26244957e7
Egglol1 <24egglal@gmail.com>
2024-12-16 19:07:25 -0500

    Temporarily allowing all origins for frontend testing

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors({
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
+      return callback(new Error(message ), true); //CHANGE TO FALSE LATER ALEX
     }
     return callback(null, true);
   }
