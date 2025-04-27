cc19be4feeb79c56409d5bf584bd99adb3c4f080
Egglol1 <24egglal@gmail.com>
2025-04-27 10:30:18 -0400

    Added ANOTHER localhost to allowedOrigins

--- index.js
+++ index.js
@@ -37,18 +37,8 @@ let allowedOrigins = [
   'http://testsite.com',
   'http://localhost:1234',
   'https://egglol1-myflix-85b3e5.netlify.app',
+  'http://localhost:4200',
 ];
-//app.use(cors({
-//  origin: (origin, callback) => {
-//    console.log('Origin: ', origin)
-//    if(!origin || allowedOrigins.includes(origin)) return callback(null, true);
-//    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-//      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-//      return callback(new Error(message ), false);
-//    }
-//    return callback(null, true);
-//  }
-//}));
 
 app.use(
   cors({
